71905419b3f9da037bc254d7fb4d0bbdef186b44
Michael Huang <huang.zha@husky.neu.edu>
2018-06-12 19:45:03 -0400

    Added Initial prototype styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter as Router, Link,Switch, Route}
+    from 'react-router-dom'
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import NavBar from "./Components/NavBar";
+import './Style.css';
+import AccountSideBar from "./Components/AccountSideBar";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const Home = () => {
+    return (
+        <div className="container">
+            <div className="container-col">
+            <AccountSideBar/>
+            </div>
+            <div className="container-main">
+                <h1> Welcome to College Counseling </h1>
+                <Link to='/'> Click Here To Search Colleges </Link></div>
+            <div className="container-col"> </div>
+        </div>
+    )
+};
+
+const App = () => {
+    return (
+        <Router>
+            <div>
+                <Route exact path='/'
+                       component={Home}/>
+                <Route exact path='/CollegeSearch'
+                       component={NavBar}/>
+            </div>
+        </Router>
+    )
+};
+const Header = () => {
+    return (
+        <div>
+            <div>
+                <NavBar/>
+            </div>
+            <div>
+                <App/>
+            </div>
+        </div>
+    )
+};
+ReactDOM.render(
+    <Header/>,
+    document.getElementById('root')
+);
