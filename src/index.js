f7b1a1809b9d6b8199e61ed0b1d7529fe6b4f774
Michael Huang <huang.zha@husky.neu.edu>
2018-06-13 17:32:46 -0400

    Working basic home page with college search

--- src/index.js
+++ src/index.js
@@ -3,44 +3,33 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Link,Switch, Route}
     from 'react-router-dom'
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import NavBar from "./Components/NavBar";
+import NavBar from "./components/NavBar";
 import './Style.css';
-import AccountSideBar from "./Components/AccountSideBar";
+import AccountSideBar from "./components/AccountSideBar";
+import CollegePage from "./components/CollegePage";
+import CollegeManager from "./components/CollegeManager";
 
-const Home = () => {
-    return (
-        <div className="container">
-            <div className="container-col">
-            <AccountSideBar/>
-            </div>
-            <div className="container-main">
-                <h1> Welcome to College Counseling </h1>
-                <Link to='/'> Click Here To Search Colleges </Link></div>
-            <div className="container-col"> </div>
-        </div>
-    )
-};
 
-const App = () => {
-    return (
-        <Router>
-            <div>
-                <Route exact path='/'
-                       component={Home}/>
-                <Route exact path='/CollegeSearch'
-                       component={NavBar}/>
-            </div>
-        </Router>
-    )
-};
+
+
 const Header = () => {
     return (
-        <div>
+        <div style={{height:'100%'}}>
             <div>
                 <NavBar/>
             </div>
-            <div>
-                <App/>
+            <div style={{height:'100%'}}>
+                <div className="container">
+                    <div className="container-col">
+                        <AccountSideBar/>
+                    </div>
+                    <div className="container-main">
+                        <CollegeManager/>
+                    </div>
+                    <div className="container-col">
+                        <AccountSideBar/>
+                    </div>
+                </div>
             </div>
         </div>
     )
