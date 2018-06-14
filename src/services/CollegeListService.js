3f38023aa99c65acafb85a72e4c41b95560b1669
Veronica Shei <shei.v@husky.neu.edu>
2018-06-14 17:31:09 -0400

    added framework for college list

---
+++ src/services/CollegeListService.js
@@ -0,0 +1,34 @@
+import ReviewService from "./ReviewService";
+
+let _singleton = Symbol();
+const LOCAL_URL = 'http://localhost:8080';
+export default class CollegeListService {
+    constructor(singletonToken) {
+        if (_singleton !== singletonToken)
+            throw new Error('Cannot instantiate directly.');
+    }
+
+    static get instance() {
+        if (!this[_singleton])
+            this[_singleton] = new CollegeListService(_singleton);
+        return this[_singleton];
+    }
+
+    createCollegeList(collegeList) {
+        return fetch(LOCAL_URL + '/api/collegeList', {
+            method: 'POST',
+            body: JSON.stringify(collegeList),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: 'include'
+        }).then(response => {
+            return response.text().then(text => {
+                if (text) {
+                    return JSON.parse(text);
+                }
+                return null;
+            });
+        });
+    }
+}
